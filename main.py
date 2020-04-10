1a10015e8f1a76e3f1c3f6011e3ab0b34b52af19
User <>
2020-04-10 22:03:06 +0300

    working dice map

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def div_image(matrix):
     :return: A new matrix, thresholded to 6 values.
     """
     zero_mat = np.zeros(matrix.shape)  # Construct a zero matrix
+    dice_map = np.zeros(matrix.shape)  # Construct a zero matrix
 
     image_top = np.max(matrix)  # Get min and max pixels
     image_bottom = np.min(matrix)
@@ -47,8 +48,21 @@ def div_image(matrix):
     delta = (image_top-image_bottom)//6
     for i in range(0, 6):
         zero_mat[matrix >= i*delta] = (255*i//6)  # Let all pixels above the threshold be one color.
+        dice_map[matrix >= i*delta] = (i+1)  # Let all pixels above the threshold be one color.
 
-    return zero_mat
+    return zero_mat, dice_map
+
+
+def avg_dice_map (dice_map , cube_res):
+    w_map, h_map = dice_map.shape
+    cubes_w = w_map//cube_res
+    cubes_h = h_map//cube_res
+    empt_mat = np.zeros((cubes_w, cubes_h))
+
+    for row in range (0,cubes_w):
+        for col in range (0,cubes_h):
+            empt_mat[row][col] = (np.sum(dice_map[row*cube_res:(row+1)*cube_res, col*cube_res:(col+1)*cube_res]))//(cube_res**2)
+    return empt_mat
 
 
 def main():
@@ -61,8 +75,10 @@ def main():
     scaled = scale_image(image_file, cube_res)
     scaled_matrix = np.asarray(scaled)
 
-    new_im = div_image(scaled_matrix)
-    Image.fromarray(new_im).show()
+    new_im, dice_map = div_image(scaled_matrix)
+    mat = avg_dice_map(dice_map,cube_res)
+    mat2 = avg_dice_map(new_im,cube_res)
+    Image.fromarray(mat2).show()
 
 
 if __name__ == "__main__":
